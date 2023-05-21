cf9a8036854c8a6b8a8b6cc971df39e0adda57f3
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 12:24:14 +0400

    import

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Models/ForeignAffairsApplication.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Models/ForeignAffairsApplication.java
@@ -1,4 +1,8 @@
 package com.codline.ForeignAffairsMinistryAPI.Models;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ForeignAffairsApplication {
 }
