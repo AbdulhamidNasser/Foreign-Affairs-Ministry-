4672efebdace36da95b20a61e670b41827652190
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 16:47:40 +0400

    import of NewsRepository

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/NewsController.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/NewsController.java
@@ -1,5 +1,6 @@
 package com.codline.ForeignAffairsMinistryAPI.Controller;
 
+import com.codline.ForeignAffairsMinistryAPI.Repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
