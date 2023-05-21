237a2f66eea3e14abd0c35d052ede47552596532
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 16:09:53 +0400

    import for public Policy updatePolicy(@PathVariable Long id, @RequestBody Policy policy) {

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
@@ -2,6 +2,7 @@ package com.codline.ForeignAffairsMinistryAPI.Controller;
 
 import com.codline.ForeignAffairsMinistryAPI.Repository.PolicyRepository;
 import com.codline.ForeignAffairsMinistryAPI.Service.Policy;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class PolicyController {
