b2a7694880bb069c3855abb1e4dd11fe29db4bbc
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 16:14:51 +0400

    getPoliciesByCountry

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
@@ -33,6 +33,12 @@ public class PolicyController {
 
 
 
+    public Iterable<Policy> getPoliciesByCountry(@PathVariable String country) {
+        return policyRepository.findByCountry(country);
+    }
+
+
+
 
 
 
