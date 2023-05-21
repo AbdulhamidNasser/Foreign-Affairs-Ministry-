66644af6caa24e42653aaf2c964b2658e05169ad
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 16:45:04 +0400

    import @Autowired

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/NewsController.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/NewsController.java
@@ -1,9 +1,12 @@
 package com.codline.ForeignAffairsMinistryAPI.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/news")
 public class NewsController {
+
+    @Autowired
 }
