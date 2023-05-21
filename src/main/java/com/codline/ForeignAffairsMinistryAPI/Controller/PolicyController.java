92b13c559f152b7da96ade6c0e3986c4c221446f
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 16:05:48 +0400

    createPolicy

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
@@ -5,4 +5,9 @@ import com.codline.ForeignAffairsMinistryAPI.Repository.PolicyRepository;
 public class PolicyController {
 
     private PolicyRepository policyRepository;
+
+
+    public Policy createPolicy(@RequestBody Policy policy) {
+        return policyRepository.save(policy);
+    }
 }
