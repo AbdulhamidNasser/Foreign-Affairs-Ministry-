e837d8794b30052fe4493e37f2a4944d341b0702
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 10:41:11 +0400

    Foreign Affairs Ministry

---
+++ src/test/java/com/codline/ForeignAffairsMinistryAPI/ForeignAffairsMinistryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codline.ForeignAffairsMinistryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignAffairsMinistryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
