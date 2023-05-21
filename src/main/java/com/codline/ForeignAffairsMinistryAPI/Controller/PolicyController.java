465a5af9893f0017a2eac3624004159ef617389b
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 15:57:12 +0400

    PolicyRepository

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/PolicyController.java
@@ -1,4 +1,8 @@
 package com.codline.ForeignAffairsMinistryAPI.Controller;
 
+import com.codline.ForeignAffairsMinistryAPI.Repository.PolicyRepository;
+
 public class PolicyController {
+
+    private PolicyRepository policyRepository;
 }
