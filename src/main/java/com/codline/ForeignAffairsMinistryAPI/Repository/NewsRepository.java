dcc40fefe17a311c9d20dcf83a8b05f68b67321c
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-21 14:43:41 +0400

    import

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Repository/NewsRepository.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Repository/NewsRepository.java
@@ -1,4 +1,6 @@
 package com.codline.ForeignAffairsMinistryAPI.Repository;
 
+import com.codline.ForeignAffairsMinistryAPI.Service.News;
+
 public interface NewsRepository extends JpaRepository<News, Long>{
 }
