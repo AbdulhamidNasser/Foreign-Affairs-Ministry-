ac633c425cdf3ff9535deb65ccf01194b9257cd4
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-24 12:02:15 +0400

    add data policy

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Models/Policy.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Models/Policy.java
@@ -1,5 +1,6 @@
 package com.codline.ForeignAffairsMinistryAPI.Models;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import javax.persistence.Entity;
 @Setter
 @Getter
 @Entity
+@Data
 public class Policy {
 
 
