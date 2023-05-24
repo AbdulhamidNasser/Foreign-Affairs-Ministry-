cfeba4151d2ea2817c3ecbfc55a82508de8e7e8b
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-24 16:13:13 +0400

    update Repository News

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Repository/PolicyRepository.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Repository/PolicyRepository.java
@@ -1,16 +1,17 @@
 package com.codline.ForeignAffairsMinistryAPI.Repository;
 
+import com.codline.ForeignAffairsMinistryAPI.Models.Policy;
 import com.codline.ForeignAffairsMinistryAPI.Service.PolicyService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PolicyRepository extends JpaRepository<PolicyService, Long> {
+public interface PolicyRepository extends JpaRepository<Policy, Integer> {
 
 
-    Iterable<PolicyService> findByCountry(String country);
+    //Iterable<PolicyService> findByCountry(String country);
 
-    Iterable<PolicyService> findByRegion(String region);
+    //Iterable<PolicyService> findByRegion(String region);
 
-    Iterable<PolicyService> findByTopic(String topic);
+   // Iterable<PolicyService> findByTopic(String topic);
 }
