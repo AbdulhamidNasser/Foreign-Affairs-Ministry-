4029c60520b6062ad99854ef9a952c98265bafdb
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-24 16:15:27 +0400

    update Repository News

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Repository/NewsRepository.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Repository/NewsRepository.java
@@ -1,5 +1,6 @@
 package com.codline.ForeignAffairsMinistryAPI.Repository;
 
+import com.codline.ForeignAffairsMinistryAPI.Models.News;
 import com.codline.ForeignAffairsMinistryAPI.Service.NewsService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
