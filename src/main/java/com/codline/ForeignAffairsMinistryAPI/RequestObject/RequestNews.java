7bdcc25a24104c18f552bf68af911aaad1f4e20c
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-25 16:50:31 +0400

    update RequestNews

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/RequestObject/RequestNews.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/RequestObject/RequestNews.java
@@ -2,6 +2,7 @@ package com.codline.ForeignAffairsMinistryAPI.RequestObject;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
