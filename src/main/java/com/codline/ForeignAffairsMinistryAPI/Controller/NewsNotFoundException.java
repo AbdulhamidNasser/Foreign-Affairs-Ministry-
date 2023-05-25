987db4cb695a7b5c4b65b008b5a99e0dc52536de
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-25 16:57:54 +0400

    update RequestNews

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/NewsNotFoundException.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Controller/NewsNotFoundException.java
@@ -2,6 +2,8 @@ package com.codline.ForeignAffairsMinistryAPI.Controller;
 
 public class NewsNotFoundException extends RuntimeException {
 
-    
+    public NewsNotFoundException(String message) {
+        super(message);
+    }
 
 }
