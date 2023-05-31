cca6d2a2aeccf952246b394b4b81751190dc5e3f
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-05-31 14:40:23 +0400

    update

--- src/main/java/com/codline/ForeignAffairsMinistryAPI/Service/NewsService.java
+++ src/main/java/com/codline/ForeignAffairsMinistryAPI/Service/NewsService.java
@@ -1,6 +1,7 @@
 package com.codline.ForeignAffairsMinistryAPI.Service;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
